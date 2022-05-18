d7152937e2ba70db7453a9a2132f50f9f6e1df58
Bruce W. Lowther <brucelowther@gmail.com>
2022-05-18 17:56:34 -0400

    Code Revision done and tests working.

--- main.py
+++ main.py
@@ -7,79 +7,104 @@ To update the script to run it on a local repo change
 Git blame increases in time with > commits in a repo: https://bugs.chromium.org/p/git/issues/detail?id=18
 """
 
-import time, os, pathlib, re
+import time
+import os
+import re
+from typing import List, Generator, Set, Optional, Tuple
+from pathlib import Path
+from collections import defaultdict
+import logging
+from pprint import pprint
 
-num_files = 0 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=os.getenv('LOGLEVEL', 'DEBUG'))
 
-def get_contributors_for_file(directory, filepath, extensions, weeks):
-    # Step 1: if the file doesn't have the correct extension, return
-    if pathlib.Path(filepath).suffix not in extensions: return []
 
-    global num_files 
-    num_files += 1 
+# Compile the re used to grab the committer once for later use.
+git_line_porcelain_action_re = re.compile('^(?P<action>committer) (?P<value>.*)$')
 
+def get_contributors_for_file(filepath: Path,
+                              weeks: Optional[int] = None) -> Set:
+    """
+    given a file, use `git blame` to identify the committer for each line in
+    the file.  Accumulate the distinct committers and return them
+    :param filepath: The file to investigate with `git blame`
+    :param weeks: Parameter to `git blame` see `--since` option
+    :return: return a set of distinct committers.
+    """
     # Step 2: If there is a weeks argument, add the since arg to the git command
-    since = "--since={weeks}.weeks -- ".format(weeks=weeks)
-    if weeks == 0: since = ""
-
+    since = f"--since={weeks}.weeks" if weeks else ""
+    directory = filepath.parent
     # Step 3: Do the git blame command and get the output
-    command = "git -C {directory} blame {since}{filepath}".format(directory=directory, filepath=filepath, since=since)
+    # use '--line-porcelain' to reduce the parsing burden
+    command = f"git -C {directory} blame --line-porcelain {since} -- {filepath}"
     # "git -C /Users/etoor/dir blame /Users/etoor/specificfile.ext"
-    all_lines  = os.popen(command).readlines()
 
     # Step 4: Get the output from all lines
-    ret = []
-    for line in all_lines:
-        # https://stackoverflow.com/questions/42539892/git-blame-see-changes-after-a-certain-date
-        if line and line[0] == "^": continue # any line before the since arg will start with ^ 
-        if "(" in line and ")" in line: 
-            blame = line.split("(")[1].split(")")[0]
-            user = re.split('[\d]{4}-[\d]{2}', blame)[0].strip()
-            ret.append(user)
+    try:
+        ret = set()
+        line_count = 0
+        for line in os.popen(command).readlines():
+            line = line.strip()
+            if porcelain_info := git_line_porcelain_action_re.search(line):
+                if porcelain_info.group('action') == 'committer':
+                    ret.add(porcelain_info.group('value'))
+        return ret
+    except UnicodeDecodeError as uedc:
+        logger.warning("could not process file: {f} error: {uedc}")
+        return set()
 
-    return ret
+def all_files_by_extension(dirpath: Path,
+                           extensions: Set[str],
+                           exclude_dirs: Optional[Set[str]]=None) -> Generator[Path,None,None]:
+    """
+    :param dirpath: The base directory for a git repository `git rev-parse --show-toplevel`
+    :param extensions: the files that have these extensions will be included.
+    :param exclude_dirs: any subdirectory by this name will be excluded. (for example, .git or .venv)  If not
+    supplied then do not exclude any
+    :return: Generator that wil provide all files in the directory and any sub-directory meeting the critera.
+    """
+    if not dirpath.exists():
+        raise ValueError(f"dirpath must exist {dirpath}")
+    exclude_dirs = exclude_dirs if exclude_dirs else []
+    for base, dirnames, files in os.walk(dirpath):
+        # don't go down directories that are excluded at any level.
+        # have to use del here on dirnames (see docs for os.walk)
+        for exclude_dir in exclude_dirs:
+            try:
+                item_idx = dirnames.index(exclude_dir)
+                del dirnames[item_idx]
+            except ValueError:
+                # excluded_dir doesn't exist in dirnames.  Throws Value error and it is ignored.
+                pass
 
-def get_all_files(dirpath): 
-    all_files = []
-
-    for dirpath, dnames, fnames in os.walk(dirpath):
-        for f in fnames: 
-            full_file = os.path.join(dirpath, f)
-            all_files.append((dirpath, full_file))
-
-    return all_files
- 
+        for file in [dirpath.joinpath(base, f).absolute() for f in files if Path(f).suffix in extensions]:
+            yield file
 
 def main():
     # Define hmap, extensions we want to get, weeks we want to read, dirpath
-    contributors_hmap = {}
-    extensions = [".go", ".py"]
-    weeks = 50  
+    contributors_hmap = defaultdict(int)
+    extensions = {".c", ".py", ".txt"}
+    exclude_directories = {'.git'}
+    weeks = 52
     start_time = time.time()
 
-    dirpath = "/Users/etoor/Desktop/open-source/go-ethereum"
+    dirpath = Path("/Users/brucelowther/src/NetHack")
 
+    print(f"Processing Git Directory:{dirpath}")
     # Step 1: Walk the file tree (get all files)
-    all_dirs_and_files = get_all_files(dirpath)
-
     # Step 2: For each file, run the git blame and get the authors of each line 
-    # Step 3: Aggregate the results of the git blame into a hashmap 
-    for dirpath, filepath in all_dirs_and_files:
-        list_of_contributors = get_contributors_for_file(dirpath, filepath, extensions, weeks)
-        for name in list_of_contributors:
-            if name in contributors_hmap: 
-                contributors_hmap[name] += 1 
-            else: 
-                contributors_hmap[name] = 1 
-
-
-    print(contributors_hmap)
-    print("Results obtained in:", time.time() - start_time, "seconds")
-    print(num_files, "files blamed")
-    total = 0 
-    for name in contributors_hmap: 
-        total += contributors_hmap[name]
-    print(total, "total lines")
+    # Step 3: Aggregate the results of the git blame into a hashmap
+    for file_index, f in enumerate(all_files_by_extension(dirpath, extensions, exclude_dirs=exclude_directories)):
+        print(f"{file_index}:{f}")
+        for name in get_contributors_for_file(f, weeks):
+            contributors_hmap[name] += 1
+    print(f"Results obtained in:{time.time() - start_time} seconds")
+    contributors_total = sum(contributors_hmap.values())
+    print(f"Contributor total lines: {contributors_total}")
+    pprint(contributors_hmap, sort_dicts=True)
 
 if __name__ == "__main__":
     main()
+
+
--- tests/main_functions.py
+++ tests/main_functions.py
@@ -13,14 +13,10 @@ def test_get_all_files():
     file_list = list(all_files_by_extension(dirpath=dirpath, extensions=extensions,
                                             exclude_dirs=directory_exclusions))
 
-    os_command = f'find {dirpath} -type f'
-    expected_files_stream = popen(os_command)
-    expected_files = expected_files_stream.readlines()
-
     assert len(file_list) == 5
 
 def test_get_contributors_for_file():
     dirpath = Path('tests/data/walk_test/test01/test03/test01.go').absolute()
     actual_result = get_contributors_for_file(dirpath, 1)
-    expected_result = {'Bruce Lowther'}
+    expected_result = {'Bruce W. Lowther'}
     assert actual_result == expected_result
